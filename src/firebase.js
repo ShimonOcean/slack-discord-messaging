ec9fc055d5d38fc4b55bae0f8a6a25dc15cee449
ShimonIslam <shimon.ocean@gmail.com>
2021-07-23 14:57:44 -0700

    Skeleton for app, cleanup extra files from react-app

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBg6TDayeCkA8XyJmaUEfKxQEb8r7Jzn5o",
+    authDomain: "slack-discord-messaging.firebaseapp.com",
+    projectId: "slack-discord-messaging",
+    storageBucket: "slack-discord-messaging.appspot.com",
+    messagingSenderId: "706710627884",
+    appId: "1:706710627884:web:f0ce8ca648ddb1d578d389"
+  };
