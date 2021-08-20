e7c1a60f5c575fb8c4e063bd45b9f90def574a57
ShimonIslam <shimon.ocean@gmail.com>
2021-08-19 18:47:24 -0700

    Comments on Redux appSlice.js

--- src/features/appSlice.js
+++ src/features/appSlice.js
@@ -9,14 +9,17 @@ export const appSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
+    // On action, change state roomId to action roomId
     enterRoom: (state, action) => {
       state.roomId = action.payload.roomId;
     },
   },
 });
 
+// Use enterRoom outside of appSlice
 export const { enterRoom } = appSlice.actions;
 
+// Access roomId from Redux Global Store
 export const selectRoomId = (state) => state.app.roomId;
 
 
